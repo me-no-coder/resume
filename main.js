345b5ed83e76a20e6630a7fc0b3185fb8c19c7aa
Mrenank <mrenankrastogi@gmail.com>
2022-03-04 01:37:45 +0530

    Edits to resume

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var navMenuAnchorTags=document.querySelectorAll('.nav-menu a');
-//console.log(navMenuAnchorTags);
 for(var i=0; i<navMenuAnchorTags.length; i++)
 {
 	navMenuAnchorTags[i].addEventListener('click', function(event)
@@ -19,4 +18,54 @@ for(var i=0; i<navMenuAnchorTags.length; i++)
 			window.scrollBy(0, 40);
 		}, 10);
 	});
+}
+
+var progressBar=document.querySelectorAll('.skill-progress>div');
+var skillsContainer=document.getElementById('skill-container');
+window.addEventListener('scroll', checkScroll);
+var animationDone=false;
+
+function initialiseBars()
+{
+	var temp="";
+	for(let bar of progressBar)
+	{
+		bar.style.width=0+'%';
+	}
+}
+
+initialiseBars();
+
+function fillBars()
+{
+	for(let bar of progressBar)
+	{
+		let targetWidth=bar.getAttribute('data-skill-level');
+		let currentWidth=0;
+		let interval=setInterval(function()
+		{
+			if(currentWidth>targetWidth)
+			{
+				clearInterval(interval);
+				return;
+			}
+			currentWidth++;
+			bar.style.width=currentWidth+'%';
+		}, 8)
+	}
+}
+
+function checkScroll()
+{
+	var coordinates=skillsContainer.getBoundingClientRect();
+	if(coordinates.top<window.innerHeight && !animationDone)
+	{
+		animationDone=true;
+		fillBars();
+	}
+	else if(coordinates.top>window.innerHeight)
+	{
+		animationDone=false;
+		initialiseBars();
+	}
 }
